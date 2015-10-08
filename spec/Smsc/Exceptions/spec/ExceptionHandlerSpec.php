fc095de3a3403abe436577bc3ed7db3ec04882f4
Roel Monnens <roel@smartbit.be>
2015-10-08 09:40:58 +0200

    add extra info in ready

--- spec/Smsc/Exceptions/spec/ExceptionHandlerSpec.php
+++ spec/Smsc/Exceptions/spec/ExceptionHandlerSpec.php
@@ -4,6 +4,7 @@ namespace spec\Smsc\Exceptions;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class ExceptionHandlerSpec
@@ -13,10 +14,10 @@ use Prophecy\Argument;
 class ExceptionHandlerSpec extends ObjectBehavior {
 
     /**
-     * @param \Psr\Log\LoggerInterface $logger
+     * @param LoggerInterface $logger
      * @param \Exception               $exception
      */
-    function it_should_handle_an_error_exception_and_log_critical(\Psr\Log\LoggerInterface $logger, \Exception $exception) {
+    function it_should_handle_an_error_exception_and_log_critical(LoggerInterface $logger, \Exception $exception) {
         $this->beConstructedWith($logger);
         $this->handle($exception);
         $logger->critical($exception)->shouldBeCalled();
